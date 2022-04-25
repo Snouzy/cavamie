1e1d784909e9baa0560ec1745d5aa2ad0a66c13d
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2022-04-25 22:56:34 +0200

    feat: first commit

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exoports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./**/*.{liquid, json}', './assets/*.liquid', './layout/*.liquid', './sections/*.liquid', './snippets/*.liquid', './templates/*.liquid', './templates/**/*.liquid'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
