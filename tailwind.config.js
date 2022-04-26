23e35c721c1228e675811f143b95c2f13290f2fa
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2022-04-26 22:41:13 +0200

    feat: contact form input color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   content: ['./**/*.{liquid, json}', './assets/*.liquid', './layout/*.liquid', './sections/*.liquid', './snippets/*.liquid', './templates/*.liquid', './templates/**/*.liquid'],
   theme: {
-    extend: {},
+    colors: {
+      'default': '#505050',
+    },
+    // extend: {},
   },
   plugins: [],
 };
