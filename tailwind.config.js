33d3212aae2b8148f62edf3c02f1cb4c28f7bc6c
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2022-04-29 01:27:46 +0200

    feat: about us ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./**/*.{liquid, json}', './assets/*.liquid', './layout/*.liquid', './sections/*.liquid', './snippets/*.liquid', './templates/*.liquid', './templates/**/*.liquid'],
   theme: {
@@ -6,6 +8,19 @@ module.exports = {
       'white': '#ffffff',
       'logo': '#ff2949'
     },
+    screens: {
+      'phone': '475px',
+        
+      'tablet': '640px',
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      'xs': '475px',
+      ...defaultTheme.screens,
+    }
     // extend: {},
   },
   plugins: [],
